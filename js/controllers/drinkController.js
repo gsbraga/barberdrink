9833d12612a7f033062340f72d8dca70ec6eedda
Victor Ferreira da Silva <toedio@yahoo.com.br>
2016-11-06 17:59:21 -0200

    logic for drinkDegree

--- js/controllers/drinkController.js
+++ js/controllers/drinkController.js
@@ -1,14 +1,14 @@
 (function() {
   let app = angular.module("barberDrinks");
   app.controller("barberDrinksController", ["$scope", "drinkService", "$http","$location", function($scope, drinkService, $http, $location) {
-    $scope.coke = true;
     $scope.calculated = false;
-    //START RefData
+    /* ### REFDATA ### */
     $http.get('/js/components/refData.json').success(function(data) {
       $scope.ingredients=data["ingredients"];
       $scope.developers=data["developers"];
+      $scope.drink=data["drink"];
     });
-    //END RefData//
+    /* ### END ### */
     $scope.validateCubaLibre = function() {
       var validated = true;
       angular.forEach($scope.ingredients, function(value, key) {
@@ -28,10 +28,16 @@
       return validated;
     }
     $scope.ingrdientsDegree = function() {
-      //if ($scope.validateCubaLibre()) {
+      //if ($scope.validateCubaLibre())
         $scope.calculated = true;
         drinkService.ingrdientsDegree($scope.ingredients);
-      //}
+        drinkService.drinkDegree($scope.ingredients, $scope.drink);
+        if ($scope.drink[0].pertinenceDegree > $scope.drink[1].pertinenceDegree) {
+          $scope.greaterIndex = 0;
+        } else {
+          $scope.greaterIndex = 1;
+        }
+        if ($scope.drink[2].pertinenceDegree > $scope.drink[$scope.greaterIndex].pertinenceDegree) $scope.greaterIndex = 2;
     }
     $scope.return = function() {
       $scope.calculated = false;
