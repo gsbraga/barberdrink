123bff62082ed6806fa8c3c7a848ce3664a5a15a
Victor Ferreira da Silva <toedio@yahoo.com.br>
2016-11-07 07:52:19 -0200

    refactor drinkService

--- js/services/drinkService.js
+++ js/services/drinkService.js
@@ -5,6 +5,9 @@
     vm.maxBetweenDegrees = function(a, b, c){
       return Math.max(a, (Math.max(b,c)));
     }
+    vm.minBetweenDegrees = function(a, b, c){
+      return Math.min(a, (Math.min(b,c));
+    }
     vm.ingrdientsDegree = function (ingredients) {
       cokeService.cokeDegree(ingredients[0]);
       pepsiService.pepsiDegree(ingredients[1]);
@@ -16,18 +19,26 @@
       vm.drinkStrongDegree(ingredients, drink);
       vm.drinkWeakDegree(ingredients, drink);
     }
+    vm.drinkWeakDegree = function(ingredients, drink) {
+      var x = 0;
+      var sodaIndex = 0;
+      var iceDegree = ingredients[3].pertinenceDegree;
+      if (ingredients[1].show) sodaIndex = 1;
+      var degreeA = 0; var degreeB = 0; var degreeC = 0;
+      degreeA = vm.minBetweenDegrees(iceDegree, ingredients[sodaIndex].weakPertinenceDegree, ingredients[2].weakPertinenceDegree);
+      degreeB = vm.minBetweenDegrees(iceDegree, ingredients[sodaIndex].weakPertinenceDegree, ingredients[2].softPertinenceDegree);
+      degreeC = vm.minBetweenDegrees(iceDegree, ingredients[sodaIndex].softPertinenceDegree, ingredients[2].weakPertinenceDegree);
+      drink[0].pertinenceDegree = vm.maxBetweenDegrees(degreeA, degreeB, degreeC);
+    }
     vm.drinkSoftDegree = function(ingredients, drink) {
       var x = 0;
       var sodaIndex = 0;
       var iceDegree = ingredients[3].pertinenceDegree;
       if (ingredients[1].show) sodaIndex = 1;
       var degreeA = 0; var degreeB = 0; var degreeC = 0;
-      degreeA = Math.min(iceDegree,(Math.min(ingredients[sodaIndex].strongPertinenceDegree,
-                                            ingredients[2].weakPertinenceDegree)));
-      degreeB = Math.min(iceDegree,(Math.min(ingredients[sodaIndex].softPertinenceDegree,
-                                             ingredients[2].softPertinenceDegree)));
-      degreeC = Math.min(iceDegree,(Math.min(ingredients[sodaIndex].weakPertinenceDegree,
-                                            ingredients[2].strongPertinenceDegree)));
+      degreeA = vm.minBetweenDegrees(iceDegree, ingredients[sodaIndex].strongPertinenceDegree, ingredients[2].weakPertinenceDegree);
+      degreeB = vm.minBetweenDegrees(iceDegree, ingredients[sodaIndex].softPertinenceDegree, ingredients[2].softPertinenceDegree);
+      degreeC = vm.minBetweenDegrees(iceDegree, ingredients[sodaIndex].weakPertinenceDegree, ingredients[2].strongPertinenceDegree);
       drink[1].pertinenceDegree = vm.maxBetweenDegrees(degreeA, degreeB, degreeC);
     }
     vm.drinkStrongDegree = function(ingredients, drink) {
@@ -36,27 +47,10 @@
       var iceDegree = ingredients[3].pertinenceDegree;
       if (ingredients[1].show) sodaIndex = 1;
       var degreeA = 0; var degreeB = 0; var degreeC = 0;
-      degreeA = Math.min(iceDegree,(Math.min(ingredients[sodaIndex].strongPertinenceDegree,
-                                            ingredients[2].softPertinenceDegree)));
-      degreeB = Math.min(iceDegree,(Math.min(ingredients[sodaIndex].strongPertinenceDegree,
-                                             ingredients[2].strongPertinenceDegree)));
-      degreeC = Math.min(iceDegree,(Math.min(ingredients[sodaIndex].softPertinenceDegree,
-                                            ingredients[2].strongPertinenceDegree)));
+      degreeA = vm.minBetweenDegrees(iceDegree, ingredients[sodaIndex].strongPertinenceDegree, ingredients[2].softPertinenceDegree);
+      degreeB = vm.minBetweenDegrees(iceDegree, ingredients[sodaIndex].strongPertinenceDegree, ingredients[2].strongPertinenceDegree);
+      degreeC = vm.minBetweenDegrees(iceDegree, ingredients[sodaIndex].softPertinenceDegree, ingredients[2].strongPertinenceDegree);
       drink[2].pertinenceDegree = vm.maxBetweenDegrees(degreeA, degreeB, degreeC);
-    }
-    vm.drinkWeakDegree = function(ingredients, drink) {
-      var x = 0;
-      var sodaIndex = 0;
-      var iceDegree = ingredients[3].pertinenceDegree;
-      if (ingredients[1].show) sodaIndex = 1;
-      var degreeA = 0; var degreeB = 0; var degreeC = 0;
-      degreeA = Math.min(iceDegree,(Math.min(ingredients[sodaIndex].weakPertinenceDegree,
-                                            ingredients[2].weakPertinenceDegree)));
-      degreeB = Math.min(iceDegree,(Math.min(ingredients[sodaIndex].weakPertinenceDegree,
-                                             ingredients[2].softPertinenceDegree)));
-      degreeC = Math.min(iceDegree,(Math.min(ingredients[sodaIndex].softPertinenceDegree,
-                                            ingredients[2].weakPertinenceDegree)));
-      drink[0].pertinenceDegree = vm.maxBetweenDegrees(degreeA, degreeB, degreeC);
     }
   });
 })();
